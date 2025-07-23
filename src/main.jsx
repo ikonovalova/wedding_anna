f573576aa32679621ab698115f6fd6a57f0e178c
Irina Konovalova <konovalova@living.ru>
2025-07-23 07:03:48 +0300

    add gp

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter  basename="/wedding_anna">
             <App />
         </BrowserRouter>
     </React.StrictMode>
