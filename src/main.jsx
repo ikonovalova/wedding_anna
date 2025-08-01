cec4ad236ea1fdb751daa4e40323ff771e36587b
Irina Konovalova <konovalova@living.ru>
2025-08-01 15:16:01 +0300

    test gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,24 +6,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter  basename="/wedding_anna/">
+        <BrowserRouter  basename="/wedding_anna">
             <App />
         </BrowserRouter>
     </React.StrictMode>
 );
-
-
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import { BrowserRouter } from 'react-router-dom';
-// import './index.css'
-// import App from './App.jsx'
-//
-// createRoot(document.getElementById('root')).render(
-//     <StrictMode>
-//         <BrowserRouter>
-//             <div>test</div>
-//             <App />
-//         </BrowserRouter>
-//     </StrictMode>,
-// )
