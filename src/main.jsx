435ac1d9afc6e13caab6e42577033a5bffe0c5e4
Irina Konovalova <konovalova@living.ru>
2025-08-01 15:03:34 +0300

    test gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter  basename="/wedding_anna">
+        <BrowserRouter  basename="/wedding_anna/">
             <App />
         </BrowserRouter>
     </React.StrictMode>
